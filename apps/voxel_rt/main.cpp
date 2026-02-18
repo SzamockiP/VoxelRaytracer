dcf9fcb16cc9156fadf81860d8be4555346ef689
SzamockiP <szamocki.piotr04@gmail.com>
2026-02-18 19:46:17 +0100

    Math methods added

--- apps/voxel_rt/main.cpp
+++ apps/voxel_rt/main.cpp
@@ -2,20 +2,20 @@
 #include "voxel_rt/core/camera.hpp"
 #include "voxel_rt/core/ray.hpp"
 #include "voxel_rt/math/vec3.hpp"
+#include "voxel_rt/math/math.hpp"
 #include "voxel_rt/io/ppm_writer.hpp"
 #include "voxel_rt/core/buffer_2d.hpp"
 
 using namespace vrt;
 int main()
 {
-	std::print("voxel_rt: hello");
 	Buffer2D<Vec3f> image_buffer{ 160,100 };
 
-	Camera camera {
-		Vec3f{0,0,0},
-		Vec3f{1.f,1.f,-1},
-		16.f/10,
-		3.14f/2
+	Camera camera{
+		Vec3f{0},
+		Vec3f{1},
+		16.f / 10,
+		deg_to_rad(90.f)
 	};
 
 	for (int y = 0; y < image_buffer.height(); ++y)
